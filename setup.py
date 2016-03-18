b045109cc6cb30d9b78dbf076bbf0ae61a520d85
Nicholas Petty <nick@ihackeverything.com>
2016-03-17 22:20:13 -0500

    ok...

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='xerial',
-    version='1.0.2b1',
+    version='1.0.3b1',
     author='Nicholas Petty',
     author_email='nick@ihackeverything.com',
-    packages=['app'],
+    packages=['xerial'],
     package_data={'':['docs/*.md','LICENSE']},
     include_package_data=True,
     url='http://github.com/nickpetty/xerial',
