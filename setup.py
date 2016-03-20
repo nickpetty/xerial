bf13003d0058a79406a5c8949cbbfff18792ea1d
Nicholas Petty <nick@ihackeverything.com>
2016-03-20 14:35:03 -0500

    Fixed build number bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xerial',
-    version='1.1.4b1',
+    version='1.1.10b1',
     author='Nicholas Petty',
     author_email='nick@ihackeverything.com',
     packages=['xerial'],
